e74bbff4f2e37d3b6a86211bad90028f77f5943d
santh <santh@comcast.net>
2018-11-15 13:31:21 -0800

    Add files via upload

--- shapes.php
+++ shapes.php
@@ -10,12 +10,19 @@ $shapes = array('I' => array(0,2,1,3,7), 'III' => array(0,0,1,2,0), 'V' => array
                 // 7th chord base positions
                 'I 7' => array(2,1,0,2,8),'III 7' => array(1,1,0,3,11), 'V 7' => array(1,0,2,2,3),'VII 7' => array(2,0,0,3,5),
                 // m7th chord base positions
-                'I m7' => array(2,0,0,2,8),'III m7' => array(1,1,0,2,11), 'V m7' => array(0,2,1,2,3),'VII m7' => array(3,1,0,4,5),
+                'I m7' => array(2,0,0,2,8),'III m7' => array(1,1,0,2,11), 'V m7' => array(0,2,1,2,3),'VII m7' => array(3,1,0,4,4),
                 // 6th chord base positions
                 'I 6' => array(0,2,3,3,7),'III 6' => array(0,2,1,2,0), 'V 6' => array(0,1,1,3,4),'VI 6' => array(2,0,0,2,5),
                 // m6th chord base positions
                 'I m6' => array(0,1,3,3,7),'III m6' => array(0,2,1,1,0), 'V m6' => array(3,1,0,4,1),'VI m6' => array(3,1,0,3,4),
                 'I 9' => array(2,1,0,2,8),'III 9' => array(2,3,1,2,0), 'V 9' => array(1,0,0,2,3),'VII 9' => array(2,2,0,3,5),'IX 9' => array(1,0,2,3,9),
+                'I 13' => array(1,0,1,1,9), 'III 13' => array(0,2,1,2,0), 'VI 13' => array(1,0,2,4,3), 'VII 13' => array(1,0,2,4,3),
+                
+                // 7+5 chord base positions
+                'I 7+5' => array(2,1,1,2,8),'III 7+5' => array(1,2,0,3,11), 'V 7+5' => array(1,0,2,3,3),'VII 7+5' => array(3,0,0,3,5),
+                // 7b5 chord base positions
+                'I 7b5' => array(3,2,0,3,7),'III 7b5' => array(1,0,0,3,11), 'V 7b5' => array(1,0,2,1,3),'VII 7b5' => array(1,0,0,3,5),
+               
                 'dim' => array(2,1,0,3,7),
                 'aug' => array(0,1,1,2,8),
                 'altI 7' => array(1,0,2,1,9),'altIII 7' => array(0,3,1,2,0), 'altV 7' => array(1,0,2,2,3),'altVII 7' => array(2,1,0,0,8),
